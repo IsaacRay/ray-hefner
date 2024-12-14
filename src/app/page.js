b0a2189e93d15cfd983d148a6f110b3b9b220e70
Isaac Ray <isaacmray1984@gmail.com>
2024-12-14 12:23:08 -0500

    adding security

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
+
+"use client"
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
 import styles from "./page.module.css";
 import {ColtonDemerit, ColtonStar, FamilyRoomLight, IsaacGarage, KathrynGarage, Logging} from "./buttons";
 import Link from 'next/link';
 
 
 export default function Home() {
+  const router = useRouter();
+  //check if the page's query string equals "kice"
+  if (typeof window !== 'undefined' && window.location.search === "?p=kice") {
+    //if it does, redirect to the groceries page
   return (
+
     <main className={styles.main}>
         <div className={styles.buttons}>
         <p>
@@ -27,6 +35,9 @@ export default function Home() {
         </div>
     </main>
   );
+}else{
+  router.push('/pattern');
+}
 }
 
 
