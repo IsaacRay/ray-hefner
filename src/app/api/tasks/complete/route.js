ae8bdd8881906821a211b8b9674efebea2657476
Isaac Ray <isaacmray1984@gmail.com>
2024-12-17 11:07:05 -0500

    trying something else

--- src/app/api/tasks/complete/route.js
+++ src/app/api/tasks/complete/route.js
@@ -5,8 +5,33 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const supabaseKey = process.env.SUPABASE_KEY;
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
+const secret_name = "SUPABASE_KEY";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const supabaseKey = response.SecretString || JSON.parse(process.env.secrets)("supabase_key");
 
 // Initialize Supabase client
 const supabase = createClient(
--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -5,11 +5,35 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
+const secret_name = "SUPABASE_KEY";
 
-  const supabaseKey = process.env.SUPABASE_KEY;//process.env.NODE_ENV !== 'development' ? JSON.parse(process.env.secrets)['supabase_key']  : process.env.SUPABASE_KEY;
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
 
+let response;
 
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const supabaseKey = response.SecretString || JSON.parse(process.env.secrets)("supabase_key");
+
+ 
 // Initialize Supabase client
 const supabase = createClient(
   "https://lzxiyzhookfqphsmrwup.supabase.co",
@@ -25,7 +49,6 @@ export async function GET(req) {
       .eq('visible', true);
 
     if (error) throw error;
-    console.log(data)
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
