548ff3ab86eacf241f4a744b156253496fa50980
Isaac Ray <isaacmray1984@gmail.com>
2024-12-17 12:15:49 -0500

    ugh

--- src/app/api/tasks/complete/route.js
+++ src/app/api/tasks/complete/route.js
@@ -1,37 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Load environment variables from .env file in local development
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
 
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
+require('dotenv').config();
 
-const secret_name = "SUPABASE_KEY";
 
-const client = new SecretsManagerClient({
-  region: "us-east-1",
-});
-
-let response;
-
-try {
-  response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-    })
-  );
-} catch (error) {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  throw error;
-}
-
-const supabaseKey = response.SecretString || JSON.parse(process.env.secrets)("supabase_key");
+const supabaseKey = process.env.SUPABASE_KEY
 
 // Initialize Supabase client
 const supabase = createClient(
