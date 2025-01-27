e45c48867feea7c5d1f1a8417963b534d0596761
Isaac Ray <isaacmray1984@gmail.com>
2025-01-27 16:17:00 -0500

    page titles

---
+++ src/app/magic-link/layout.js
@@ -0,0 +1,9 @@
+import SignInPage from "./page";
+
+export const metadata = {
+    title: 'Superbowl Squares',
+  };
+
+  export default function ChildPage() {
+    return <SignInPage />;
+  }
---
+++ src/app/squares/layout.js
@@ -0,0 +1,9 @@
+import SquaresPage from "./page";
+
+export const metadata = {
+    title: 'Superbowl Squares',
+  };
+
+  export default function ChildPage() {
+    return <SquaresPage />;
+  }
