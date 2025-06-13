1c1e3ec29ca690db768dee5d1b9a021e8e3481fd
Isaac Ray <isaacmray1984@gmail.com>
2025-06-13 11:28:33 -0400

    totals count

--- src/app/api/weekly-totals/route.js
+++ src/app/api/weekly-totals/route.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://lzxiyzhookfqphsmrwup.supabase.co"
+const supabaseKey = process.env.SUPABASE_KEY
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const dynamic = 'force-dynamic';
